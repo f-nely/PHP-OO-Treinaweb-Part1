c0fda138c9cb5cb699efa034aa9e9b4d075e96d3
nely de faria <nelly.nf54@gmail.com>
2020-08-05 12:58:19 -0300

    Instancia classe Cliente

--- php-oo-tw/index.php
+++ php-oo-tw/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'src/Classes/Produto.php';
+require_once 'src/Classes/Cliente.php';
 
 $prod1 = new Produto();
 $prod1->titulo = "Skol";
@@ -13,4 +14,13 @@ $prod2->titulo = "Itaipava";
 $prod2->preco = 1.90;
 
 var_dump($prod1, $prod2);
+
+$cli = new Cliente();
+$cli->nome = "Harris";
+$cli->idade = 40;
+$cli->endereco = "São Paulo";
+$cli->telefone = "(11) - xxxxx-xxxx";
+$cli->compra();
+
+
 
