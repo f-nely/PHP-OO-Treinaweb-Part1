61915baa6dad5367d986bdf778becc5a57bfe4bb
nely de faria <nelly.nf54@gmail.com>
2020-08-07 18:53:12 -0300

    Cria funções

--- php-oo-tw/src/Classes/Produto.php
+++ php-oo-tw/src/Classes/Produto.php
@@ -8,4 +8,14 @@ class Produto
     public $descricao = "Cerveja Brasileira";
 
     public $preco;
+
+    public function defineCodigoBarras(string $codigo): void
+    {
+        $this->codigoBarras = $codigo; //Propriedade em tempo de execução
+    }
+
+    public function acessaCodigoBarras()
+    {
+        return $this->codigoBarras;
+    }
 }
