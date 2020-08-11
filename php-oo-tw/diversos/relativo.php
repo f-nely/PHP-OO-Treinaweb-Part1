3fd4ffaf6ffa26eacbd222b93de6324ab409da0c
nely de faria <nelly.nf54@gmail.com>
2020-08-11 15:21:31 -0300

    Inclui classe Transporte

--- php-oo-tw/diversos/relativo.php
+++ php-oo-tw/diversos/relativo.php
@@ -2,6 +2,7 @@
 
 require_once '../src/Email/Adaptadores/Mailgun/Adaptador.php';
 require_once '../src/Email/Envio.php';
+require_once '../src/Email/Transporte.php';
 
 $email = new \App\Email\Envio();
 
