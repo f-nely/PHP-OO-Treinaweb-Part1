f19e96ee9acf14fc1c8acaea151047a1106b0fb7
nely de faria <nelly.nf54@gmail.com>
2020-08-11 15:08:35 -0300

    Cria função envia()

--- php-oo-tw/src/Email/Envio.php
+++ php-oo-tw/src/Email/Envio.php
@@ -6,7 +6,12 @@ const VERSION = 1.0;
 
 class Envio
 {
-
+    public function envia(): void
+    {
+        //namaspace completo
+        $adaptador = new \App\Email\Adaptadores\Mailgun\Adaptador;
+        var_dump($adaptador);
+    }
 }
 
 function valida(string $email): void
